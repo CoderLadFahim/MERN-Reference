1eec0d9e9df900841cfef9e2fe36a5e225856661
Fahim <fahimalemroz@gmail.com>
2022-03-05 08:25:28 +0600

    Added the function body of registerUser controller

--- server/controllers/operatorController.js
+++ server/controllers/operatorController.js
@@ -1,4 +1,4 @@
-const brcypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 const Operator = require('../models/operatorModel');
@@ -6,23 +6,56 @@ const Operator = require('../models/operatorModel');
 // DESC: Registers an operator
 // ENDPOINT: POST  /api/operators/
 // ACCESS: Public
-const registerOperator = (req, res) => {
-	res.json({ message: 'Operator registered' });
-};
+const registerOperator = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error('Missing fields');
+	}
+
+	// checking to see if operator exists
+	const operatorExists = await Operator.findOne({ email });
+
+	// Throwing an error if user does exist
+	if (operatorExists) {
+		res.status(400);
+		throw new Error('Operator you are trying to create already exists');
+	}
+
+	// Hasing the password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const operator = await Operator.create({
+		name,
+		email,
+		password: hashedPassword,
+	});
+
+	if (operator) {
+		res.status(201).json({
+			_id: operator.id,
+			name: operator.name,
+			email: operator.email,
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid operator data');
+	}
+});
 
 // DESC: Authenticate an operator
 // ENDPOINT: POST  /api/operators/login/
 // ACCESS: Public
-const loginOperator = (req, res) => {
-	res.json({ message: 'Operator Login' });
-};
+const loginOperator = asyncHandler(async (req, res) => {});
 
 // DESC: Get an operator's data
 // ENDPOINT: GET  /api/operators/me/
 // ACCESS: Public
-const getOperatorData = (req, res) => {
+const getOperatorData = asyncHandler(async (req, res) => {
 	res.json({ message: 'Operator data' });
-};
+});
 
 module.exports = {
 	registerOperator,
--- server/models/operatorModel.js
+++ server/models/operatorModel.js
@@ -11,18 +11,6 @@ const operatorSchema = mongoose.Schema(
 			required: [true, 'Please add an email address'],
 			unique: true,
 		},
-		gender: {
-			type: String,
-			required: [true, 'Please add one of the two genders'],
-		},
-		religion: {
-			type: String,
-			required: [true, 'Please state your chosen faith'],
-		},
-		age: {
-			type: String,
-			required: [true, 'Please state your age'],
-		},
 		password: {
 			type: String,
 			required: [true, 'Please add a password'],
